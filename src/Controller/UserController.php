7fce205be36d787b890ab13766ce1dc10e02e92c
Antonino <45453064+AntoninoM90@users.noreply.github.com>
2024-05-11 15:54:04 +0200

    Moved "/user" routes to "/admin/user" and set up the role access control

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/user')]
+#[Route('/admin/user')]
 class UserController extends AbstractController
 {
     #[Route('/', name: 'app_user_index', methods: ['GET'])]
