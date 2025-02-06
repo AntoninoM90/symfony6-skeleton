e3f6fd2b244e236760b7a20afbde3c6275796465
Antonino <45453064+AntoninoM90@users.noreply.github.com>
2025-02-06 20:23:28 +0100

    Updated the "Profile" controller and twig template

--- src/Controller/User/ProfileController.php
+++ src/Controller/User/ProfileController.php
@@ -11,8 +11,10 @@ class ProfileController extends AbstractController
     #[Route('/profile', name: 'app_profile')]
     public function index(): Response
     {
+        $user = $this->getUser();
+
         return $this->render('user/profile/index.html.twig', [
-            'controller_name' => 'ProfileController',
+            'user' => $user
         ]);
     }
 }
